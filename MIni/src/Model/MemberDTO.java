b9d3a28e0279c0422cd253dd13698137b2432f65
smhrd <smhrd@DESKTOP-D22MMHC>
2022-09-05 12:46:43 +0900

    DTO

--- MIni/src/Model/MemberDTO.java
+++ MIni/src/Model/MemberDTO.java
@@ -1,5 +1,22 @@
 package Model;
 
 public class MemberDTO {
+	private String id;
+	private String pw;
+	private String nick;
 
+	
+	
+	public MemberDTO(String id, String nick) {
+		this.id = id;
+		this.nick = nick;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getNick() {
+		return nick;
+	}
+	
+	
 }
