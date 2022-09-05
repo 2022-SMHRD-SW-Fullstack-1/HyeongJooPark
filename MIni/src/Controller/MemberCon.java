a8449c950d6eb738ce46dce7af933e8668cb175e
smhrd <smhrd@DESKTOP-2G65ABC>
2022-09-05 12:17:10 +0900

    회원정보 Controller DAO에서 insert, login DTO에서 rank 담을 ArrayList nick, Score rownum 조회 바람

---
+++ MIni/src/Controller/MemberCon.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import model.MemberDTO;
+
+public class MemberCon {
+	
+	//컨트롤러 역할을 진행할 클래스
+	
+	//View ↔ DAO(DB의 로직)
+	
+	MemberDAO dao = new MemberDAO();
+	
+	int cnt = 0;
+	
+	public int conInsert(String id, String pw, String nick) {
+		//view가 넘겨준 회원가입에 대한 정보들을 DAO로 연결해주는 메소드
+		cnt = dao.insert(id, pw, nick);
+		
+		return cnt;	
+	}
+	
+	public ArrayList<MemberDTO> conRank() {
+		
+		ArrayList<MemberDTO> totalList = new ArrayList<>();
+		
+		totalList = dao.select();
+		
+		return totalList;
+	}
+	
+	//로그인을 진행하기 위한 Controller 요청
+	public String conLogin(String id, String pw) {
+		
+		String nick = dao.login(id, pw);
+				
+		return nick;
+	}
+	
+
+}
