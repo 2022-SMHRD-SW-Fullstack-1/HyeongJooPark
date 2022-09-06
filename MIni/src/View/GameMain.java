7807049b5afd53032577c73e80245f31847a571f
smhrd <smhrd@DESKTOP-2G65ABC>
2022-09-06 09:18:08 +0900

    로그인 수정

--- MIni/src/View/GameMain.java
+++ MIni/src/View/GameMain.java
@@ -53,80 +53,80 @@ public class GameMain {
 
 				if(nick != null) {
 					System.out.println("환영합니다! " + nick + " 님");					
+					
+					//로그인 후 화면 
+					System.out.print("[1]게임시작 [2]게임설명보기 >> ");
+					int inputLog = sc.nextInt();
+					
+					if(inputLog == 1) {
+						//게임화면
+						
+						int score = 0; //누적 점수
+						int count = 3; //남은 생명
+						int round = 1; //몇번째 문제인지
+						System.out.println("========난이도 선택========");
+						System.out.print("[1]Easy [2]Normal [3]Hard >> ");
+						mc.selectLevel(sc.nextInt());
+						mc.GenerateRandomVariable();
+						while(true) {
+							if(round<=3) {
+								mc.play();
+								mc.stop();				
+								System.out.println("========"+(round)+"번째 문제========");
+								System.out.println("패스를 원할 시 p를 입력하세요");
+								System.out.print("정답 입력 >>");
+								String ans = sc.next();
+								if(ans.equals(mc.answer())) {
+									score = mc.score(ans);
+									round++;
+									System.out.println("정답입니다!");
+									System.out.println("현재 획득한 점수 : "+score);
+									mc.next();
+									continue;
+								}else if(ans.equals("p")) {
+									mc.next();
+									round++;
+									continue;
+								}else {
+									System.out.println("틀렸습니다!");
+									count--;
+									System.out.println("남은 횟수 : "+count+"/3");	
+								}
+								if(count==0) {
+									System.out.println("====================");
+									System.out.println("You're dead");
+								}
+							}
+							System.out.println("====================");
+							
+							//랭크 보여줄 자리
+							if(round>3) {
+								System.out.println("게임 종료");
+								System.out.println("다시 도전하시겠습니까?");
+								System.out.print("[1]Yes [2]No");
+								int retry = sc.nextInt();
+								if(retry==1) {
+									count=3;
+									score=0;
+									round=1;
+									mc.GenerateRandomVariable();
+									continue;
+								}			
+								break;
+							}
+						}
+						System.out.println("초기화면으로 돌아갑니다");
+						
+						
+						
+					}else if(inputLog == 2) {
+						//게임설명화면
+						
+						
+						
+					}
 				}else {
 					System.out.println("로그인에 실패 하셨습니다");
-				}
-
-				//로그인 후 화면 
-				System.out.print("[1]게임시작 [2]게임설명보기 >> ");
-				int inputLog = sc.nextInt();
-
-				if(inputLog == 1) {
-					//게임화면
-
-					int score = 0; //누적 점수
-					int count = 3; //남은 생명
-					int round = 1; //몇번째 문제인지
-					System.out.println("========난이도 선택========");
-					System.out.print("[1]Easy [2]Normal [3]Hard >> ");
-					mc.selectLevel(sc.nextInt());
-					mc.GenerateRandomVariable();
-					while(true) {
-						if(round<=3) {
-							mc.play();
-							mc.stop();				
-							System.out.println("========"+(round)+"번째 문제========");
-							System.out.println("패스를 원할 시 p를 입력하세요");
-							System.out.print("정답 입력 >>");
-							String ans = sc.next();
-							if(ans.equals(mc.answer())) {
-								score = mc.score(ans);
-								round++;
-								System.out.println("정답입니다!");
-								System.out.println("현재 획득한 점수 : "+score);
-								mc.next();
-								continue;
-							}else if(ans.equals("p")) {
-								mc.next();
-								round++;
-								continue;
-							}else {
-								System.out.println("틀렸습니다!");
-								count--;
-								System.out.println("남은 횟수 : "+count+"/3");	
-							}
-							if(count==0) {
-								System.out.println("====================");
-								System.out.println("You're dead");
-							}
-						}
-						System.out.println("====================");
-
-						//랭크 보여줄 자리
-						if(round>3) {
-							System.out.println("게임 종료");
-							System.out.println("다시 도전하시겠습니까?");
-							System.out.print("[1]Yes [2]No");
-							int retry = sc.nextInt();
-							if(retry==1) {
-								count=3;
-								score=0;
-								round=1;
-								mc.GenerateRandomVariable();
-								continue;
-							}			
-							break;
-						}
-					}
-					System.out.println("초기화면으로 돌아갑니다");
-
-
-
-				}else if(inputLog == 2) {
-					//게임설명화면
-
-
-
 				}
 
 			}else if(input == 3) {
